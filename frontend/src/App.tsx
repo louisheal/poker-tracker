334bec46fdeb66fb79ec69b0f46396ac80d0f22f
Louis Heal <louisheal@live.co.uk>
2025-11-15 14:32:29 +0000

    add and display button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
 import "./App.css";
 import SixMaxTable from "./components/pokerTables/SixMaxTable";
 import HandContainer from "./components/Hand";
+import { useState } from "react";
 
 function App() {
+  const [btnPos, setBtnPos] = useState(0);
+
+  const nextHand = () => {
+    setBtnPos((prev) => (prev + 1) % 6);
+  };
+
   return (
     <>
-      <HandContainer />
-      <SixMaxTable />
+      <SixMaxTable btnPos={btnPos} />
+      <HandContainer nextHand={nextHand} />
     </>
   );
 }
